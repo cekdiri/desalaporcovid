2fff0df20bb3585aad66c87acd0de1d95f014093
teknosuper <ponyow@gmail.com>
2020-04-15 00:28:02 +0700

    update video panduan petugas posko

--- views/site/index.php
+++ views/site/index.php
@@ -51,6 +51,21 @@
         </div>
       </div>
 
+      <div class="col-md-6">
+        <div class="box box-primary box-solid">
+          <div class="box-header with-border">
+            <i class="fa fa-video-camera"></i>
+
+            <h3 class="box-title">Video Panduan Penggunaan Untuk Petugas Posko</h3>
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/KCzxkAhPI74" frameborder="0" allowfullscreen=""></iframe>
+            </div>        
+          </div>
+        </div>
+      </div>
 
     </div>   
 
